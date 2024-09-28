510bd09c0ada5a5f25c1f88469a38849134d8d60
Lucas Barque <lucasbarquedasilva@hotmail.com>
2024-09-27 21:29:26 -0400

    feat: add suspense error compiling next on vercel

--- src/components/Dish.tsx
+++ src/components/Dish.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
@@ -25,33 +27,35 @@ export function Dish({ id, title, description, price, medias }: DishProps) {
   const restaurantId = searchParams.get('restaurantId');
 
   return (
-    <Link
-      href={`/dish/${restaurantId}/${menuId}/${id}`}
-      className="rounded-3xl bg-white p-5"
-    >
-      <div className="flex gap-4">
-        {medias.length > 0 && (
-          <Image
-            src={medias[0].filename}
-            alt={medias[0].title}
-            width={96}
-            height={96}
-            className="h-24 w-24 rounded-2xl object-cover"
-          />
-        )}
+    <Suspense>
+      <Link
+        href={`/dish/${restaurantId}/${menuId}/${id}`}
+        className="rounded-3xl bg-white p-5"
+      >
+        <div className="flex gap-4">
+          {medias.length > 0 && (
+            <Image
+              src={medias[0].filename}
+              alt={medias[0].title}
+              width={96}
+              height={96}
+              className="h-24 w-24 rounded-2xl object-cover"
+            />
+          )}
 
-        <div className="flex-1">
-          <div className="text-heading-xs text-black">{title}</div>
-          <p className="pt-1 text-regular-xs text-gray-400">{description}</p>
+          <div className="flex-1">
+            <div className="text-heading-xs text-black">{title}</div>
+            <p className="pt-1 text-regular-xs text-gray-400">{description}</p>
+          </div>
         </div>
-      </div>
-      <div className="pt-3 text-right text-heading-sm text-black">
-        <span className="text-yellow-500">R$</span>{' '}
-        {new Intl.NumberFormat('pt-BR', {
-          currency: 'BRL',
-          minimumFractionDigits: 2,
-        }).format(price / 100)}
-      </div>
-    </Link>
+        <div className="pt-3 text-right text-heading-sm text-black">
+          <span className="text-yellow-500">R$</span>{' '}
+          {new Intl.NumberFormat('pt-BR', {
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+          }).format(price / 100)}
+        </div>
+      </Link>
+    </Suspense>
   );
 }
